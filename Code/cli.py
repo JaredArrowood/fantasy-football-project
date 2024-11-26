b86a02c8f6f6bb51172e6d3c79c307d1b7dd4fdd
Hoa Nguyen <hoowahwin@gmail.com>
2024-11-26 09:33:06 -0600

    setting up the inital CLI for login

--- Code/cli.py
+++ Code/cli.py
@@ -1,3 +1,40 @@
 #create a command line interface for the application
-import argparse
-import sys
+import sqlite3
+
+CONNECTION_STRING = 'fantasy_league.db'
+
+def login(db):
+    print("Login failed. Please try again.")
+    return False
+
+def register(db):
+    return True
+
+if __name__ == "__main__":
+    db_connection = sqlite3.connect(CONNECTION_STRING)
+    db = db_connection.cursor()
+    
+    login_success = False
+
+    while(login_success != True):
+        print("1. Login")
+        print("2. Register")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            login_success = login(db)
+            if not login_success:
+                continue
+        elif choice == "2":
+            login_success = register(db)    
+            if not login_success:
+                continue
+        elif choice == "3":
+            db_connection.close()
+            exit()
+
+        print("Welcome to the CLI Fantasy League!")
+    
+    db_connection.close()
+
---
+++ Code/create_db.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+# won't be needed once the database is created
+conn = sqlite3.connect('fantasy_league.db')
+
