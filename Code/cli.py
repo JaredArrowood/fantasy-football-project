8c61fad18bc6792a9523ab9a18c268c18d4e7b5b
JaredArrowood <jarrow1@lsu.edu>
2024-11-27 12:24:58 -0600

    starting query tests

--- Code/cli.py
+++ Code/cli.py
@@ -48,10 +48,83 @@ if __name__ == "__main__":
 
     while(True):
         print("4. Logout")
+        print("5. View Roster")
+        print("6. View Player Statistics")
+
         choice = input("Enter your choice: ")
 
         if choice == "4":
             print(f"=== Thank you for using the CLI Fantasy League, {USER}! ===")
             main_menu(db)
+        elif choice == "5":
+            while(True):
+                print("1. View Roster")
+                print("2. Add Player")
+                print("3. Drop Player")
+                choice = input("Enter your choice: ")
+                if choice == "1":
+                    print("Viewing Roster")
+                    db.execute('''WITH team_players(player_id) AS 
+                            (SELECT player_id 
+                            FROM team, player, user
+                            WHERE player.team_id = team.team_id AND team.email = user.email)
+
+                                SELECT player_name, position, real_team
+                                    FROM team_players, player
+                                    WHERE team_players.player_id = player.player_id
+                            ''')
+                    results = db.fetchall()
+                    for row in results:
+                        print(row)
+                elif choice == "2":
+                    print("Adding Player")
+                    quit = False
+                    while(True):
+                        player_name = input("Enter the player's name: (Q to quit)")
+                        if(player_name == "Q"):
+                            quit = True
+                            break
+                        #check if the player is already on a team
+                        db.execute('''SELECT team_id
+                                    FROM player
+                                    WHERE player_name = ?''', (player_name,))
+                        condition = db.fetchone()
+                        if(condition == None):
+                            print("Player Avaiable!")
+                            break
+                        else:
+                            print("Player already in a team. Select another player.")
+                    if(quit):
+                        continue
+                    else:
+                        db.execute('''
+                            UPDATE player
+                            SET team_id = (SELECT team_id
+                                            FROM team, user, player
+                                            WHERE team.email = user.email AND player.player_name = ?)''', (player_name,))
+                        db_connection.commit()
+                        print(f"{player_name} added to your team.")
+            
+
+        elif choice == "6":
+            print("Viewing Player Statistics")
+
+            while(True):
+                player_name = input("Enter the player's name: (Q to Quit)")
+                if player_name == "Q":
+                    break
+                db.execute('''SELECT * 
+                            FROM player_statistics
+                            WHERE player_id = (SELECT player_id
+                                                FROM player
+                                                WHERE player_name = ?)''', (player_name,))
+                results = db.fetchall()
+                if len(results) == 0:
+                    print("Player not found.")
+                else:
+                    for row in results:
+                        print(row)
+            
+                
         else:
             print("Invalid choice. Please try again.")
