dc1eb826b493a40c8aa35a913397e759bf42dfc4
Hoa Nguyen <hoowahwin@gmail.com>
2024-11-30 10:25:37 -0600

    refactored while-loop functionality into its own functions

--- Code/cli.py
+++ Code/cli.py
@@ -1,14 +1,15 @@
 #create a command line interface for the application
 import sqlite3
+
 class User():
     def __init__(self, email, username, password):
         self.email = email
         self.username = username
         self.password = password
-    
 CONNECTION_STRING = 'fantasy_league.db'
 USER = User("", "", "")
 
+# region User Functions
 def check_if_email_exists(db, email):
     db.execute('''SELECT email
                 FROM user
@@ -83,6 +84,7 @@ def register(db):
     
     print(f"Welcome to the CLI Fantasy League, {USER.username}!")
     return True
+# endregion
 
 # displays the login/signup portion of the CLI
 def main_menu(db):
@@ -110,155 +112,156 @@ def main_menu(db):
             print("Invalid choice. Please try again.")
             continue
 
+def roster_menu(db):
+    while(True):
+        print("===================================")
+        print("1. View Roster")
+        print("2. Add Player")
+        print("3. Drop Player")
+        print("Q. Quit")
+        
+        choice = input("Enter your choice: ")
+
+        #Should return the player's names, positions, and real team that are on the selected user's team
+        #players not on a team have a value of 0 in the team_id column
+        if choice == "1":
+            print("===================================")
+            print("> Viewing Roster")
+            db.execute('''WITH team_players(player_id) AS 
+                    (SELECT player_id 
+                    FROM team, player
+                    WHERE player.team_id = team.team_id AND team.email = ?)
+
+                        SELECT player_name, position, real_team
+                            FROM team_players, player
+                            WHERE team_players.player_id = player.player_id
+                    ''', (USER.email,))
+            results = db.fetchall()
+            for row in results:
+                print(row)
+        #Should update the player's team_id to the selected user's team_id, if it already has a different
+        #team_id, thhen it will ask the user to pick another player
+        elif choice == "2":
+            print("===================================")
+            print("> Adding Player")
+            quit = False
+            while(not quit):
+                player_name = input("Enter the player's name: (Q to quit) ")
+                if(player_name == "Q"):
+                    quit = True
+                    continue
+                # check if the player exists
+                db.execute('''SELECT player_name
+                            FROM player
+                            WHERE player_name = ?''', (player_name,))
+                condition = db.fetchone()
+                if condition is None:
+                    print("> Player not found. Please try again.")
+                    continue
+                #check if the player is already on a team
+                db.execute('''SELECT team_id
+                            FROM player
+                            WHERE player_name = ?''', (player_name,))
+                condition = db.fetchone()[0]
+                if(condition == 0):
+                    print("> Player Avaiable!")
+                    #update the player relation to change their team_id to the user's team_id
+                    db.execute('''
+                        UPDATE player
+                        SET team_id = (SELECT team_id
+                                FROM team
+                                WHERE team.email = ?)
+                            WHERE player_name = ?''', (USER.email,player_name,))
+                    db_connection.commit()
+                    print(f"> {player_name} added to your team.")
+                    quit = True
+                else:
+                    print("> Player already in a team. Select another player.")
+        elif choice == "3":
+            #drop player
+            print("===================================")
+            print("> Dropping Player")
+            #Should update the player's team_id to 0, if it already has a team_id of 0, then it will ask the user to pick another player
+            quit = False
+            while(not quit):
+                player_name = input("Enter the player's name: (Q to quit) ")
+                if(player_name == "Q"):
+                    quit = True
+                    continue
+                # check if the player exists
+                db.execute('''SELECT player_name, team_id
+                            FROM player
+                            WHERE player_name = ?''', (player_name,))
+                condition = db.fetchone()
+                if condition is None:
+                    print("> Player not found. Please try again.")
+                    continue
+                #check if the player is already on a team
+                if(condition[1] == 0):
+                    print("> Player already not on a team. Select another player.")
+                    continue
+                #check if the player is on the user's team, if not, then the cannot drop the player
+                elif(condition[1] != 0):
+                    db.execute('''SELECT team_id
+                                FROM team
+                                WHERE email = ?''', (USER.email,))
+                    team_id = db.fetchone()[0]
+                    if(condition[1] != team_id):
+                        print("> Player not on your team. Please try again.")
+                        continue
+                    else:
+                        quit = True
+                        #update the player relation to change their team_id to 0
+                        db.execute('''
+                            UPDATE player
+                            SET team_id = 0
+                            WHERE player_name = ?''', (player_name,))
+                        db_connection.commit()
+                        print(f"> {player_name} dropped from your team.")
+        elif choice == "Q":
+            break
+        else:
+            print("===================================")            
+            print("> Invalid choice. Please try again.")
+
+def player_statistics(db):
+    print("> Viewing Player Statistics")
+    #Should return the selected player's statistics
+    while(True):
+        player_name = input("Enter the player's name: (Q to Quit) ")
+        if player_name == "Q":
+            break
+        db.execute('''SELECT * 
+                    FROM player_statistics
+                    WHERE player_id = (SELECT player_id
+                                        FROM player
+                                        WHERE player_name = ?)''', (player_name,))
+        results = db.fetchall()
+        if len(results) == 0:
+            print("> Player not found.")
+        else:
+            for row in results:
+                print("TODO: IMPROVE PRINTING OF PLAYER STATISTICS")
+                print(row)
+
 if __name__ == "__main__":
     db_connection = sqlite3.connect(CONNECTION_STRING)
     db = db_connection.cursor()
     main_menu(db)
 
     while(True):
-        print("4. Logout")
-        print("5. View Roster")
-        print("6. View Player Statistics")
+        print(f"1. {USER.username}'s Roster")
+        print("2. View Player Statistics")
+        print("L. Logout")
 
         choice = input("Enter your choice: ")
 
-        if choice == "4":
+        if choice == "L":
             print(f"=== Thank you for using the CLI Fantasy League, {USER.username}! ===")
             main_menu(db)
-        elif choice == "5":
-            while(True):
-                print("1. View Roster")
-                print("2. Add Player")
-                print("3. Drop Player")
-                print("Q. Quit")
-                choice = input("Enter your choice: ")
-                if choice == "1":
-                    print("Viewing Roster")
-                    #Should return the player's names, positions, and real team that are on the selected user's team
-                    #players not on a team have a value of 0 in the team_id column
-                    db.execute('''WITH team_players(player_id) AS 
-                            (SELECT player_id 
-                            FROM team, player
-                            WHERE player.team_id = team.team_id AND team.email = ?)
-
-                                SELECT player_name, position, real_team
-                                    FROM team_players, player
-                                    WHERE team_players.player_id = player.player_id
-                            ''', (USER.email,))
-                    results = db.fetchall()
-                    for row in results:
-                        print(row)
-                elif choice == "2":
-                    print("Adding Player")
-                    #Should update the player's team_id to the selected user's team_id, if it already has a different
-                    #team_id, thhen it will ask the user to pick another player
-                    quit = False
-                    while(True):
-                        player_name = input("Enter the player's name: (Q to quit)")
-                        if(player_name == "Q"):
-                            quit = True
-                            break
-                        # check if the player exists
-                        db.execute('''SELECT player_name
-                                    FROM player
-                                    WHERE player_name = ?''', (player_name,))
-                        condition = db.fetchone()
-                        if condition is None:
-                            print("Player not found. Please try again.")
-                            continue
-                        #check if the player is already on a team
-                        db.execute('''SELECT team_id
-                                    FROM player
-                                    WHERE player_name = ?''', (player_name,))
-                        condition = db.fetchone()[0]
-                        if(condition == 0):
-                            print("Player Avaiable!")
-                            break
-                        else:
-                            print("Player already in a team. Select another player.")
-                    if(quit):
-                        break
-                    else:
-                        #update the player relation to change their team_id to the user's team_id
-                        db.execute('''
-                            UPDATE player
-                            SET team_id = (SELECT team_id
-                                   FROM team
-                                   WHERE team.email = ?)
-                                WHERE player_name = ?''', (USER.email,player_name,))
-                        db_connection.commit()
-                        print(f"{player_name} added to your team.")
-                elif choice == "3":
-                    #drop player
-                    print("Dropping Player")
-                    #Should update the player's team_id to 0, if it already has a team_id of 0, then it will ask the user to pick another player
-                    quit = False
-                    while(True):
-                        player_name = input("Enter the player's name: (Q to quit)")
-                        if(player_name == "Q"):
-                            quit = True
-                            break
-                        # check if the player exists
-                        db.execute('''SELECT player_name
-                                    FROM player
-                                    WHERE player_name = ?''', (player_name,))
-                        condition = db.fetchone()
-                        if condition is None:
-                            print("Player not found. Please try again.")
-                            continue
-                        #check if the player is already on a team
-                        db.execute('''SELECT team_id
-                                    FROM player
-                                    WHERE player_name = ?''', (player_name,))
-                        condition = db.fetchone()[0]
-                        #if the team_id is already 0, then they are not on a team
-                        if(condition == 0):
-                            print("Player already not on a team. Select another player.")
-                            continue
-                        #check if the player is on the user's team, if not, then the cannot drop the player
-                        elif(condition != 0):
-                            db.execute('''SELECT team_id
-                                        FROM team
-                                        WHERE email = ?''', (USER.email,))
-                            team_id = db.fetchone()[0]
-                            if(condition != team_id):
-                                print("Player not on your team. Please try again.")
-                                continue
-                        if(quit):
-                            break
-                        else:
-                            #update the player relation to change their team_id to 0
-                            db.execute('''
-                                UPDATE player
-                                SET team_id = 0
-                                WHERE player_name = ?''', (player_name,))
-                            db_connection.commit()
-                            print(f"{player_name} dropped from your team.")
-                elif choice == "Q":
-                    break
-                else:
-                    print("Invalid choice. Please try again.")
-            
-
-        elif choice == "6":
-            print("Viewing Player Statistics")
-            #Should return the selected player's statistics
-            while(True):
-                player_name = input("Enter the player's name: (Q to Quit)")
-                if player_name == "Q":
-                    break
-                db.execute('''SELECT * 
-                            FROM player_statistics
-                            WHERE player_id = (SELECT player_id
-                                                FROM player
-                                                WHERE player_name = ?)''', (player_name,))
-                results = db.fetchall()
-                if len(results) == 0:
-                    print("Player not found.")
-                else:
-                    for row in results:
-                        print(row)
-            
-                
+        elif choice == "1":
+            roster_menu(db)
+        elif choice == "2":
+            player_statistics(db)
         else:
-            print("Invalid choice. Please try again.")
+            print("> Invalid choice. Please try again.")
